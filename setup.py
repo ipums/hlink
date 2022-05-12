ipums/hlink

commit 153907941547cffbdee7f00ac9f194cf55296b1d

Upgrade to Jinja2 v3
- This introduced an error that I had some trouble tracking down. The
  PackageLoader couldn't load some of the packages because they didn't
  have a templates/ subdirectory. So I've added some empty templates/
  directories to fix the issue.

setup.py
@@ -30,7 +30,7 @@ package_data["hlink.linking"].append("table_definitions.csv")
 install_requires = [
     "colorama==0.4.1",
     "ipython==7.0.1",
-    "Jinja2==2.10",
+    "Jinja2==3.0.3",
     "numpy==1.19.5",
     "pandas==1.1.5",
     "pyspark==3.2.1",

commit 2314df6204a654d171a90d9b18bf4c06f5871638

Copy all templates/ directories over when running pip install .
- This fixes a bug I introduced in commit 153907941547cffbdee7f00ac9f194cf55296b1d

setup.py
@@ -12,6 +12,11 @@ packages_with_templates = [
     "hlink.linking.preprocessing",
     "hlink.linking.matching",
     "hlink.linking.hh_matching",
+    "hlink.linking.model_exploration",
+    "hlink.linking.hh_model_exploration",
+    "hlink.linking.reporting",
+    "hlink.linking.training",
+    "hlink.linking.hh_training",
 ]
 
 package_data = {"hlink.spark": ["jars/hlink_lib-assembly-1.0.jar"]}