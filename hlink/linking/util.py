f91b87b7e72c88a10a10859bd42cf9d3192b84df
rileyh <rileyh@umn.edu>
2022-08-30 09:24:34 -0500

    Modify spark_shuffle_partitions_heuristic() to clamp its value to the range [200..10000]
    - This prevents very large datasets from requesting ~100000+ partitions

--- hlink/linking/util.py
+++ hlink/linking/util.py
@@ -1,12 +1,18 @@
 from math import ceil
 
 
+MIN_PARTITIONS = 200
+MAX_PARTITIONS = 10000
+
+
 def spark_shuffle_partitions_heuristic(dataset_size):
     """Calculate how many partitions to request from Spark based on dataset size.
 
     This is a heuristic / approximation of how many partitions should be requested
     from Spark so that hlink performs well. The minimum number of partitions
-    returned is 200, the default for Spark.
+    returned is 200, the default for Spark. The maximum number returned is 10,000.
     """
     partitions_approx = ceil(dataset_size / 25000)
-    return max(200, partitions_approx)
+    clamped_below = max(MIN_PARTITIONS, partitions_approx)
+    clamped = min(MAX_PARTITIONS, clamped_below)
+    return clamped
