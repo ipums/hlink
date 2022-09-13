d0eb8caf592e56f66421a7feaebe3a62df5a66af
rileyh <rileyh@umn.edu>
2022-09-13 16:16:23 -0500

    Bump the version to 3.2.7, regenerate Sphinx docs to match new version

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ dev_requires = [
 
 setup(
     name="hlink",
-    version="3.2.6",
+    version="3.2.7",
     packages=find_packages(),
     description="Fast supervised pyspark record linkage software",
     package_data=package_data,
