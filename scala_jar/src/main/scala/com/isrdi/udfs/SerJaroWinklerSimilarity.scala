0e481d8ed0e8b1e37f948798036a4e49afb2b7a6
rileyh <rileyh@umn.edu>
2022-11-29 12:44:45 -0600

    [#56] Add a comment with some explanation of the situation to the Scala code

--- scala_jar/src/main/scala/com/isrdi/udfs/SerJaroWinklerSimilarity.scala
+++ scala_jar/src/main/scala/com/isrdi/udfs/SerJaroWinklerSimilarity.scala
@@ -7,6 +7,15 @@ package com.isrdi.udfs
 import org.apache.commons.text.similarity._
 
 class SerJaroWinklerSimilarity extends JaroWinklerSimilarity with Serializable {
+  // JaroWinklerSimilarity.apply("", "") returns 1.0, but for our use case it makes
+  // more sense for two empty strings to have similarity 0.0.
+  //
+  // By my understanding, the comparison of two empty strings is essentially
+  // undefined. To me it makes sense to directly apply the definition of
+  // Jaro-Winkler similarity to two empty strings and get a similarity of 0.
+  // But this has the unfortunate side effect of making the distance between two
+  // empty strings 1 - 0 = 1. So I understand the decision to make the
+  // similarity 1 and the distance 0 as well.
   override def apply(left: CharSequence, right: CharSequence): java.lang.Double = {
     if (left == "" && right == "") {
       0.0
