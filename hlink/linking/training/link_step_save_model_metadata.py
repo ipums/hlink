1ccc9f7135402316cd2c6c20fcf9046448d10d57
rileyh <rileyh@umn.edu>
2023-10-18 19:06:55 +0000

    [#60] Remove some now-redundant if statements from training step 3

--- hlink/linking/training/link_step_save_model_metadata.py
+++ hlink/linking/training/link_step_save_model_metadata.py
@@ -32,59 +32,41 @@ class LinkStepSaveModelMetadata(LinkStep):
             )
             return
 
-        if "feature_importances" in config[training_conf]:
-            if config[training_conf]["feature_importances"]:
-
-                # retrieve the saved chosen model
-                print("Loading chosen ML model...")
-                model_path = Path(config["spark_tmp_dir"]) / "chosen_model"
-                try:
-                    plm = PipelineModel.load(str(model_path))
-                except:
-                    print(
-                        "Model not found!  You might need to run step_2 to generate and train the chosen model if you haven't already done so."
-                    )
-
-                # make look at the features and their importances
-                print("Retrieving model feature importances or coefficients...")
-                try:
-                    feature_imp = plm.stages[-2].coefficients
-                except:
-                    try:
-                        feature_imp = plm.stages[-2].featureImportances
-                    except:
-                        print(
-                            "This model doesn't contain a coefficient or feature importances parameter -- check chosen model type."
-                        )
-                    else:
-                        label = "Feature importances"
-                else:
-                    feature_imp = feature_imp.round(4)
-                    label = "Coefficients"
-
-                varlist = self.task.spark.table(
-                    f"{table_prefix}features_list"
-                ).toPandas()
-                for i in varlist["idx"]:
-                    varlist.at[i, "score"] = feature_imp[i]
-                varlist.sort_values("score", ascending=False, inplace=True)
-                vl = self.spark.createDataFrame(varlist)
-                vl.write.mode("overwrite").saveAsTable(
-                    f"{table_prefix}feature_importances"
-                )
-
-                print(
-                    f"{label} have been saved to the Spark table '{table_prefix}feature_importances'."
-                )
-                print(varlist)
-
-            else:
-                print(
-                    f"'feature_importances' not set == true in '{training_conf}' section of config! Calculation of feature importances or coefficients not completed!"
-                )
-        else:
+        # retrieve the saved chosen model
+        print("Loading chosen ML model...")
+        model_path = Path(config["spark_tmp_dir"]) / "chosen_model"
+        try:
+            plm = PipelineModel.load(str(model_path))
+        except:
             print(
-                f"'feature_importances' not included and set == true in '{training_conf}' section of config! Calculation of feature importances or coefficients not completed!"
+                "Model not found!  You might need to run step_2 to generate and train the chosen model if you haven't already done so."
             )
 
-        self.task.spark.sql("set spark.sql.shuffle.partitions=200")
+        # make look at the features and their importances
+        print("Retrieving model feature importances or coefficients...")
+        try:
+            feature_imp = plm.stages[-2].coefficients
+        except:
+            try:
+                feature_imp = plm.stages[-2].featureImportances
+            except:
+                print(
+                    "This model doesn't contain a coefficient or feature importances parameter -- check chosen model type."
+                )
+            else:
+                label = "Feature importances"
+        else:
+            feature_imp = feature_imp.round(4)
+            label = "Coefficients"
+
+        varlist = self.task.spark.table(f"{table_prefix}features_list").toPandas()
+        for i in varlist["idx"]:
+            varlist.at[i, "score"] = feature_imp[i]
+        varlist.sort_values("score", ascending=False, inplace=True)
+        vl = self.spark.createDataFrame(varlist)
+        vl.write.mode("overwrite").saveAsTable(f"{table_prefix}feature_importances")
+
+        print(
+            f"{label} have been saved to the Spark table '{table_prefix}feature_importances'."
+        )
+        print(varlist)
