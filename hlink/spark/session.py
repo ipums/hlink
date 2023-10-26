e49284547a2c58a23cbf6db037574616d4b93c06
balch027 <balch027@gp2.pop.umn.edu>
2023-10-26 17:14:40 +0000

    Removing the session thingy

--- hlink/spark/session.py
+++ hlink/spark/session.py
@@ -29,8 +29,7 @@ class SparkConnection:
         self.python = python
 
     def spark_conf(self, executor_cores, executor_memory, driver_memory, cores):
-        # Was getting an error where hlink.spark.__file__ return None and everything breaks
-        spark_package_path = os.path.dirname(hlink.spark.session.__file__)
+        spark_package_path = os.path.dirname(hlink.spark.__file__)
         jar_path = os.path.join(
             spark_package_path, "jars", "hlink_lib-assembly-1.0.jar"
         )
