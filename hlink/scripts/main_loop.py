1f72e95cbea1874b7307f6717c8994a9a366471b
rileyh <rileyh@umn.edu>
2023-11-01 21:32:54 +0000

    [#122] Make Main.prompt and Main.intro instance variables
    We'll need to change Main.prompt depending on the name of the config file
    passed to Main.

--- hlink/scripts/main_loop.py
+++ hlink/scripts/main_loop.py
@@ -47,9 +47,6 @@ def split_and_check_args(expected_count):
 class Main(Cmd):
     """Main program which handles user input. See https://docs.python.org/3/library/cmd.html for more information."""
 
-    prompt = "hlink $ "
-    intro = "Welcome to hlink. Type ? to list commands and q to quit.\n"
-
     def __init__(
         self,
         link_run,
@@ -57,6 +54,8 @@ class Main(Cmd):
     ):
         self.link_run = link_run
         self.spark = self.link_run.spark
+        self.prompt = "hlink $ "
+        self.intro = "Welcome to hlink. Type ? to list commands and q to quit.\n"
 
         if start_task is None:
             self.current_link_task = self.link_run.preprocessing
