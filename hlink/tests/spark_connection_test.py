2c7eee96ef009ea21a8785836c572e140754f695
rileyh <rileyh@umn.edu>
2024-10-09 16:42:49 +0000

    [#153] Add an optional app_name argument to SparkConnection
    This allows choosing an app name other than "linking".

--- hlink/tests/spark_connection_test.py
+++ hlink/tests/spark_connection_test.py
@@ -14,3 +14,20 @@ def test_app_name_defaults_to_linking(tmp_path: Path) -> None:
     spark = connection.local(cores=1, executor_memory="1G")
     app_name = spark.conf.get("spark.app.name")
     assert app_name == "linking"
+
+
+def test_app_name_argument(tmp_path: Path) -> None:
+    derby_dir = tmp_path / "derby"
+    warehouse_dir = tmp_path / "warehouse"
+    tmp_dir = tmp_path / "tmp"
+    connection = SparkConnection(
+        derby_dir,
+        warehouse_dir,
+        tmp_dir,
+        sys.executable,
+        "test",
+        app_name="test_app_name",
+    )
+    spark = connection.local(cores=1, executor_memory="1G")
+    app_name = spark.conf.get("spark.app.name")
+    assert app_name == "test_app_name"
