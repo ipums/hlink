a51f20f1e08792c0fe318296bf05c8a9eebcfdba
rileyh <rileyh@umn.edu>
2024-11-14 15:27:06 -0600

    [#161] Add a test for xgboost classifier support
    This test is currently failing if you have xgboost installed. If you don't have
    xgboost installed, it skips itself to prevent failures due to missing packages
    and dependencies.

---
+++ hlink/tests/core/classifier_test.py
@@ -0,0 +1,23 @@
+import pytest
+
+from hlink.linking.core.classifier import choose_classifier
+
+try:
+    import xgboost
+except ModuleNotFoundError:
+    xgboost_available = False
+else:
+    xgboost_available = True
+
+@pytest.mark.skipif(not xgboost_available, reason="requires the xgboost library")
+def test_choose_classifier_supports_xgboost():
+    """
+    If the xgboost module is installed, then choose_classifier() supports a model
+    type of "xgboost".
+    """
+    params = {
+        "max_depth": 2,
+        "eta": 0.5,
+    }
+    classifier = choose_classifier("xgboost", params, "match")
+    assert classifier.getLabelCol() == "match"
