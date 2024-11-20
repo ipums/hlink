a4f353471a5655ce12c6d82324d875668cb23818
rileyh <rileyh@umn.edu>
2024-11-20 09:30:13 -0600

    [#162] Implement RenameVectorAttributes and make it more flexible via some extra params

--- hlink/linking/transformers/rename_vector_attributes.py
+++ hlink/linking/transformers/rename_vector_attributes.py
@@ -1,6 +1,6 @@
 from pyspark import keyword_only
 from pyspark.ml import Transformer
-from pyspark.ml.param.shared import HasInputCol
+from pyspark.ml.param.shared import HasInputCol, Param, Params, TypeConverters
 from pyspark.sql import DataFrame
 
 
@@ -17,26 +17,57 @@ class RenameVectorAttributes(Transformer, HasInputCol):
     passing the feature vector to LightGBM for training.
     """
 
+    strsToReplace: Param[list[str]] = Param(
+        Params._dummy(),
+        "strsToReplace",
+        "Substrings to replace in the vector attribute names.",
+        typeConverter=TypeConverters.toListString,
+    )
+
+    replaceWith: Param[str] = Param(
+        Params._dummy(),
+        "replaceWith",
+        "The string to replace removed substrings.",
+        typeConverter=TypeConverters.toString,
+    )
+
     @keyword_only
-    def __init__(self, inputCol: str | None = None) -> None:
+    def __init__(
+        self,
+        *,
+        inputCol: str | None = None,
+        strsToReplace: str | None = None,
+        replaceWith: str | None = None,
+    ) -> None:
         super(RenameVectorAttributes, self).__init__()
         kwargs = self._input_kwargs
         self.setParams(**kwargs)
 
     @keyword_only
-    def setParams(self, inputCol: str | None = None):
+    def setParams(
+        self,
+        *,
+        inputCol: str | None = None,
+        strsToReplace: str | None = None,
+        replaceWith: str | None = None,
+    ):
         kwargs = self._input_kwargs
         return self._set(**kwargs)
 
     def _transform(self, dataset: DataFrame) -> DataFrame:
         metadata = dataset.schema[self.getInputCol()].metadata
         attributes_by_type = metadata["ml_attr"]["attrs"]
+        to_replace = self.getOrDefault("strsToReplace")
+        replacement_str = self.getOrDefault("replaceWith")
 
         # The attributes are grouped by type, which may be numeric, binary, or
         # nominal. We don't care about the type here; we'll just rename all of
         # the attributes.
         for _attribute_type, attributes in attributes_by_type.items():
             for attribute in attributes:
-                attribute["name"] = attribute["name"].replace(":", "_")
+                for substring in to_replace:
+                    attribute["name"] = attribute["name"].replace(
+                        substring, replacement_str
+                    )
 
         return dataset
--- hlink/tests/transformers_test.py
+++ hlink/tests/transformers_test.py
@@ -12,9 +12,29 @@ def test_rename_vector_attributes(spark: SparkSession) -> None:
     assembler = VectorAssembler(
         inputCols=["A", "regionf_0:namelast_jw"], outputCol="vectorized"
     )
-    remove_colons = RenameVectorAttributes(inputCol="vectorized")
+    remove_colons = RenameVectorAttributes(
+        inputCol="vectorized", strsToReplace=[":"], replaceWith="_"
+    )
     transformed = remove_colons.transform(assembler.transform(df))
 
     attrs = transformed.schema["vectorized"].metadata["ml_attr"]["attrs"]["numeric"]
     attr_names = [attr["name"] for attr in attrs]
     assert attr_names == ["A", "regionf_0_namelast_jw"]
+
+
+def test_rename_vector_attributes_multiple_replacements(spark: SparkSession) -> None:
+    df = spark.createDataFrame(
+        [[1, 2], [3, 4]], schema=["column1*has*stars", "column2*multiple/symbols"]
+    )
+
+    assembler = VectorAssembler(
+        inputCols=["column1*has*stars", "column2*multiple/symbols"], outputCol="vector"
+    )
+    rename_attrs = RenameVectorAttributes(
+        inputCol="vector", strsToReplace=["*", "/"], replaceWith=""
+    )
+    transformed = rename_attrs.transform(assembler.transform(df))
+
+    attrs = transformed.schema["vector"].metadata["ml_attr"]["attrs"]["numeric"]
+    attr_names = [attr["name"] for attr in attrs]
+    assert attr_names == ["column1hasstars", "column2multiplesymbols"]
